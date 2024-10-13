4087f19628660e2f9c890d26d3741068670c7583
Nis3D <103100755+Nis3D@users.noreply.github.com>
2024-10-13 21:03:08 +0800

    Installing mongoose dotenv valid -url, adding .env file  and created a model and schema

---
+++ models/urlmodel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const urlSchema = new mongoose.Schema({
+  original_url: { type: String, required: true },
+  short_url: { type: Number, required: true },
+});
+
+const URL = mongoose.model('URL', urlSchema);
+
+module.exports = URL;
